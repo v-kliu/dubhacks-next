28d0d614fe6b87727f50e401088bba8c18c4af31
Hriesha Popat <hriesha.p@gmail.com>
2026-09-14 20:33:10 +0530

    add batch 5 badminton photo to gallery

--- src/components/PhotoGallerySection.tsx
+++ src/components/PhotoGallerySection.tsx
@@ -36,9 +36,9 @@ const PhotoGallerySection: React.FC = () => {
       alt: 'Sparks founders demoing at their Demo Day booth'
     },
     {
-      filename: 'hike.jpg',
-      caption: 'turns out we CAN touch grass! (Batch 4 hike) ⛰️',
-      alt: 'Team building hike'
+      filename: 'badminton.jpg',
+      caption: 'turns out we CAN touch grass! (Batch 5 retreat badminton) 🏸',
+      alt: 'Batch 5 playing badminton on the grass at the retreat'
     },
     {
       filename: 'koel_labs.jpg',
